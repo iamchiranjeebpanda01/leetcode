b2ab1bc168081e261ee163ff05efaf6efe0d5c60
iamchiranjeebpanda01 <chiranjeebpanda01@gmail.com>
2024-01-03 10:19:28 +0530

    twoSum sol1

---
+++ Blind75/twoSum.ts
@@ -0,0 +1,23 @@
+//! Desc: https://leetcode.com/problems/two-sum/description/
+
+//* Solution 1 (Time: O(n^2), Auxiliary Space: O(1))
+function twoSum(nums: number[], target: number): number[] {
+    let x = 0;
+    let y = 1;
+    let returnVal;
+
+    while(x <= nums.length-1){
+        while(y <= nums.length-1){
+            if(nums[x]+nums[y] === target){
+                returnVal = [x,y];
+            }
+            else{
+                y++;
+            }
+        }
+        x++;
+        y = x+1;
+    }
+
+    return returnVal;
+};
